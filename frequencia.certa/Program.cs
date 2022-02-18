bc095dfae59986e929158f830a59f4b412bc6b54
55499 <55499@DUDAZANOTTO>
2022-02-18 16:26:08 -0300

    Add project files.

---
+++ frequencia.certa/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace frequencia.certa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string strQuantosnumeros = Console.ReadLine();
+            int quantidade = Convert.ToInt32(strQuantosnumeros);
+
+            int[] arrayNumeros = new int[quantidade];
+            for (int a = 0; a < quantidade; a++)
+            {
+                Console.WriteLine("Digite um número:");
+                string strNumero = Console.ReadLine();
+                int numero = Convert.ToInt32(strNumero);
+                arrayNumeros[a] = numero;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
